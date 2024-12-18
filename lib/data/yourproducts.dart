590a7b46e3c9ddc12cb2ecf94ce309d1b2f7b528
Pratham Sharma <pratham016914934@gmail.com>
2024-12-18 22:15:37 +0545

    minor changes

--- lib/data/yourproducts.dart
+++ lib/data/yourproducts.dart
@@ -23,42 +23,42 @@ List<Product> yourProduct = [
       p_image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDm1mnD_n-EAuZ71TQcxzTWf-iAmpqrNvtndmzOs8V877NccSIxMbSQxc&s"),
   Product(
-      p_id: 1,
+      p_id: 4,
       p_name: "pant",
       price: 10,
        p_desc: "this is a description",
       p_image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwh7H7Gucw-iH7Y5ywE7vW3-HJkYFU6nddv7jAFa56mPVqqJtot6Lc_-U&s"),
   Product(
-      p_id: 2,
+      p_id: 5,
       p_name: "shirt",
       price: 20,
        p_desc: "this is a description",
       p_image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0GavUAxF-7QTfadwFocZWi2inJaxt8NGBz37fW522FlYGI2LNQ_ioEBw&s"),
   Product(
-      p_id: 3,
+      p_id: 6,
       p_name: "shoes",
       price: 29,
        p_desc: "this is a description",
       p_image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDm1mnD_n-EAuZ71TQcxzTWf-iAmpqrNvtndmzOs8V877NccSIxMbSQxc&s"),
   Product(
-      p_id: 1,
+      p_id: 7,
       p_name: "pant",
       price: 10,
        p_desc: "this is a description",
       p_image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwh7H7Gucw-iH7Y5ywE7vW3-HJkYFU6nddv7jAFa56mPVqqJtot6Lc_-U&s"),
   Product(
-      p_id: 2,
+      p_id: 8,
       p_name: "shirt",
       price: 20,
        p_desc: "this is a description",
       p_image:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0GavUAxF-7QTfadwFocZWi2inJaxt8NGBz37fW522FlYGI2LNQ_ioEBw&s"),
   Product(
-      p_id: 3,
+      p_id: 9,
       p_name: "shoes",
       price: 29,
        p_desc: "this is a description",
